ecefaa2968eddab29aab2b7146efdb2b7965c559
Steve Crockett <crockett95@gmail.com>
2014-07-07 11:24:40 -0500

    Finished tutorial

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,15 @@
  * Controller of the mytodoApp
  */
 angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [
-    	'Item 1',
-    	'Item 2',
-    	'Item 3',
-    ];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore && todosInStore.split('\n') || [];
+
+    $scope.$watch('todos', function () {
+      localStorageService.add('todos', $scope.todos.join('\n'));
+    }, true);
+
     $scope.addTodo = function () {
     	if ( $scope.todo !== '' && $scope.todos.indexOf($scope.todo) === -1 ) {
     		$scope.todos.push($scope.todo);
